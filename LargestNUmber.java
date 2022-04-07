8a7a05e840430f1e1cbb92b66fd74995c36db573
vishnupumar <96655395+vishnupumar@users.noreply.github.com>
2022-04-07 12:48:37 +0530

    LargestNum

---
+++ LargestNUmber.java
@@ -0,0 +1,28 @@
+package proj2;
+
+
+public class LargestNUmber {
+	     
+		public static void main(String[] args) {
+			
+			int a,b,c;
+			
+			a=25;
+			b=30;
+			c=29;
+			
+			if(a>=b && a>=c) {
+				
+			  System.out.println(a);	
+			}
+			else if (b>=a && b>=c) {
+				
+				System.out.println(b);
+			}
+			else {
+				System.out.println(c);
+			}
+		}
+	}
+
+
